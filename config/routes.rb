addf5bc7b189de1d75fee50ec43c604f354d0dc5
tiennh1995 <nguyen.huu.tien@framgia.com>
2016-07-04 11:15:57 +0700

    login logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: "users/registrations"
+  }
+  root "static_pages#home"
+  get "help" => "static_pages#help"
+  get "about" => "static_pages#about"
 
-  root "static_pages/home"
-  get "help" => "static_pages/help"
-  get "about" => "static_pages/about"
+  namespace :admin do
+    root "users#index"
+  end
 end
