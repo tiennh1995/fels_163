7011165febf365b1bbe41f5f26c7d6b25cfb00bd
tiennh1995 <nguyen.huu.tien@framgia.com>
2016-07-05 10:26:51 +0700

    admin new word answer

--- app/controllers/admin/words_controller.rb
+++ app/controllers/admin/words_controller.rb
@@ -10,4 +10,37 @@ class Admin::WordsController < ApplicationController
 
   def show
   end
+
+  def new
+    @categories = Category.paginate page:
+      params[:page], per_page: Settings.per_page
+    @word.answers.build
+  end
+
+  def create
+    if check_word? && @word.save
+      flash[:success] = t "success"
+      redirect_to [:admin, @word]
+    else
+      @categories = Category.paginate page:
+        params[:page], per_page: Settings.per_page
+      flash[:danger] = t "danger"
+      render :new
+    end
+  end
+
+  private
+  def word_params
+    params.require(:word).permit :title, :category_id, :image,
+      answers_attributes: [:id, :title, :is_correct, :_destroy]
+  end
+
+  def check_word?
+    unless @word.answers.nil?
+      @word.answers.each do |answer|
+        return true if answer.is_correct?
+      end
+    end
+    false
+  end
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -3,4 +3,10 @@ class Word < ActiveRecord::Base
 
   has_many :results
   has_many :answers
+  validates :title, uniqueness: true
+
+  mount_uploader :image, ImageUploader
+
+  accepts_nested_attributes_for :answers,
+    reject_if: lambda {|a| a[:title].blank?}, allow_destroy: true
 end
